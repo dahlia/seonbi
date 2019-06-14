3ec241e80cf079cbcf17fc372feeb097e2e24cf1
Hong Minhee <hong.minhee@gmail.com>
2019-06-15 08:34:22 +0900

    Hangul jamo

---
+++ src/Text/Seonbi/Hangul.hs
@@ -0,0 +1,84 @@
+module Text.Seonbi.Hangul
+    ( JamoTriple
+    , fromJamoTriple
+    , isHangulSyllable
+    , toJamoTriple
+    ) where
+
+-- | A triple of an initial consonant, a vowel, and an optional final consonant.
+type JamoTriple = (Char, Char, Maybe Char)
+
+-- | Checks if a character is a hangul letter and a complete syllable.
+--
+-- >>> isHangulSyllable '가'
+-- True
+-- >>> isHangulSyllable 'ㄱ'
+-- False
+isHangulSyllable :: Char -> Bool
+isHangulSyllable c =
+    c >= '\xac00' && c <= '\xd7a3';
+
+syllableBase :: Int
+syllableBase = 0xac00
+
+initialBase :: Int
+initialBase = 0x1100
+
+vowelBase :: Int
+vowelBase = 0x1161
+
+finalBase :: Int
+finalBase = 0x11a7
+
+vowelCount :: Int
+vowelCount = 21;
+
+finalCount :: Int
+finalCount = 28;
+
+-- | Takes a complete hangul syllable apart into consonants and a vowel.
+-- Returns 'Nothing' for non-hangul letters.
+--
+-- >>> toJamoTriple '가'
+-- Just ('\4352','\4449',Nothing)
+-- >>> toJamoTriple '글'
+-- Just ('\4352','\4467',Just '\4527')
+-- >>> toJamoTriple 'A'
+-- Nothing
+toJamoTriple :: Char -> Maybe JamoTriple
+toJamoTriple c
+  | isHangulSyllable c = Just
+      ( toEnum $ initialBase + ((syllable `div` finalCount) `div` vowelCount)
+      , toEnum $ vowelBase + ((syllable `div` finalCount) `mod` vowelCount)
+      , case syllable `mod` finalCount of
+          0 -> Nothing
+          f -> Just $ toEnum (finalBase + f)
+      )
+  | otherwise = Nothing
+  where
+    syllable :: Int
+    syllable = fromEnum c - syllableBase
+
+-- | Composes hangul jamo triple into a hangul syllable.
+--
+-- >>> fromJamoTriple ('ᄀ', 'ᅡ', Nothing)
+-- Just '\44032'
+-- >>> fromJamoTriple ('ᄀ', 'ᅳ', Just 'ᆯ')
+-- Just '\44544'
+fromJamoTriple :: JamoTriple -> Maybe Char
+fromJamoTriple (initial, vowel, final)
+  | initialIndex < 0 = Nothing
+  | initialIndex > 18 = Nothing
+  | vowelIndex < 0 = Nothing
+  | vowelIndex > 20 = Nothing
+  | finalIndex < 0 = Nothing
+  | finalIndex > 27 = Nothing
+  | otherwise = Just $ toEnum $ syllableBase +
+      (initialIndex * vowelCount + vowelIndex) * finalCount + finalIndex
+  where
+    initialIndex :: Int
+    initialIndex = fromEnum initial - initialBase
+    vowelIndex :: Int
+    vowelIndex = fromEnum vowel - vowelBase
+    finalIndex :: Int
+    finalIndex = maybe 0 (\ f -> fromEnum f - finalBase) final
---
+++ test/Text/Seonbi/HangulSpec.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Text.Seonbi.HangulSpec (spec) where
+
+import Test.Hspec
+
+import Text.Seonbi.Hangul
+
+
+spec :: Spec
+spec = do
+    specify "isHangulSyllable" $ do
+        '가' `shouldSatisfy` isHangulSyllable
+        '글' `shouldSatisfy` isHangulSyllable
+        'A' `shouldNotSatisfy` isHangulSyllable
+        '?' `shouldNotSatisfy` isHangulSyllable
+        '字' `shouldNotSatisfy` isHangulSyllable
+    describe "toJamoTriple" $ do
+        it "returns only initial cosonant and vowel if there is no batchim" $
+            toJamoTriple '가' `shouldBe` Just ('ᄀ', 'ᅡ', Nothing)
+        it "returns all of triple if there is a batchim" $ do
+            toJamoTriple '글' `shouldBe` Just ('ᄀ', 'ᅳ', Just 'ᆯ')
+            toJamoTriple '를' `shouldBe` Just ('ᄅ', 'ᅳ', Just 'ᆯ')
+        it "returns Nothing for non-Hangul letters" $ do
+            toJamoTriple 'A' `shouldBe` Nothing
+            toJamoTriple '?' `shouldBe` Nothing
+            toJamoTriple '字' `shouldBe` Nothing
+    specify "fromJamoTriple" $ do
+        fromJamoTriple ('ᄀ', 'ᅡ', Nothing) `shouldBe` Just '가'
+        fromJamoTriple ('ᄀ', 'ᅳ', Just 'ᆯ') `shouldBe` Just '글'
+        fromJamoTriple ('ᄅ', 'ᅳ', Just 'ᆯ') `shouldBe` Just '를'
+        fromJamoTriple ('ᄓ', 'ᅳ', Nothing) `shouldBe` Nothing
+        fromJamoTriple ('ᄀ', 'ᅶ', Nothing) `shouldBe` Nothing
+        fromJamoTriple ('ᄀ', 'ᅳ', Just 'ᅡ') `shouldBe` Nothing
+        fromJamoTriple ('ᄀ', 'ᅳ', Just 'ᇇ') `shouldBe` Nothing
