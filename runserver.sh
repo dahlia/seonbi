53ffd56ef309526409fac26a081b3ea40a4c2560
Hong Minhee <hong.minhee@gmail.com>
2020-06-24 01:09:59 +0900

    Follow the bleeding-edge (master) version

--- runserver.sh
+++ runserver.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 set -e
-if [[ ! -f seonbi-api ]]; then
-  if [[ ! -f jq-linux64 ]]; then
-    wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-    chmod +x jq-linux64
-  fi
-  version="$(curl -v https://api.github.com/repos/dahlia/seonbi/releases/latest | ./jq-linux64 -r .tag_name)"
-  wget "https://github.com/dahlia/seonbi/releases/download/$version/seonbi-$version.linux-x86_64.tar.bz2"
-  tar xvfj "seonbi-$version.linux-x86_64.tar.bz2"
+
+if [ "$(uname -s)" = "Darwin" ]; then
+  ga_os=macos
+  jq_plat=osx-amd64
+else
+  ga_os=ubuntu
+  jq_plat=linux64
 fi
+
+if [[ ! -f seonbi-api ]]; then
+  if [[ ! -f jq ]]; then
+    wget -O jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-$jq_plat"
+    chmod +x jq
+  fi
+  auth="Authorization: token $GITHUB_TOKEN"
+  artifacts_url="$(curl -v --header "$auth" \
+                     https://api.github.com/repos/dahlia/seonbi/actions/workflows/25075/runs \
+                  | ./jq -r '.workflow_runs[0].artifacts_url')"
+  download_url="$(curl -v --header "$auth" "$artifacts_url" \
+                 | ./jq -r --arg ga_os "$ga_os" \
+                     '.artifacts | map(select(.name | startswith("binaries-" + $ga_os + "-"))) | sort_by(.name) | .[-1].archive_download_url')"
+  wget \
+    --content-disposition \
+    --trust-server-names \
+    --header "$auth" \
+    "$download_url"
+  unzip binaries-$ga_os-*.zip
+  rm binaries-$ga_os-*.zip
+  chmod +x seonbi seonbi-*
+fi
+
 ./seonbi-api -p "$1" -o '*'
