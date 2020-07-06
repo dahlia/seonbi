041add715d6d32af53c645ca95908dcd522e3528
Hong Minhee <hong.minhee@gmail.com>
2020-07-07 02:32:00 +0900

    Download binaries from gh-pages
    See also 9c4580ba08a01f5d8555f0df04b442d5513d3e4f

--- runserver.sh
+++ runserver.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
-set -e
+set -evx
 
 if [ "$(uname -s)" = "Darwin" ]; then
-  ga_os=macos
-  jq_plat=osx-amd64
+  os=macos-x86_64
 else
-  ga_os=ubuntu
-  jq_plat=linux64
+  os=linux-x86_64
 fi
 
+download_url="https://dahlia.github.io/seonbi/dists/latest/seonbi.$os.tar.bz2"
+
 if [[ ! -f seonbi-api ]]; then
-  if [[ ! -f jq ]]; then
-    wget -O jq "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-$jq_plat"
-    chmod +x jq
-  fi
-  auth="Authorization: token $GITHUB_TOKEN"
-  artifacts_url="$(curl -v --header "$auth" \
-                     https://api.github.com/repos/dahlia/seonbi/actions/workflows/25075/runs \
-                  | ./jq -r '.workflow_runs[0].artifacts_url')"
-  download_url="$(curl -v --header "$auth" "$artifacts_url" \
-                 | ./jq -r --arg ga_os "$ga_os" \
-                     '.artifacts | map(select(.name | startswith("binaries-" + $ga_os + "-"))) | sort_by(.name) | .[-1].archive_download_url')"
-  wget \
-    --content-disposition \
-    --trust-server-names \
-    --header "$auth" \
-    "$download_url"
-  unzip binaries-$ga_os-*.zip
-  rm binaries-$ga_os-*.zip
+  wget "$download_url"
+  name="$(basename "$download_url")"
+  tar xvfj "$name"
+  rm "$name"
   chmod +x seonbi seonbi-*
 fi
 
